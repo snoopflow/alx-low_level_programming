68075427b4ce8ce0d5c004747d23164f78cf2b37
snoopflow <mojolajesuolubode@gmail.com>
2022-09-08 21:13:53 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-/**
+/** 
  * main - Prints the sizeof many var types
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-        char a;
+	char a;
 	int b;
 	long int c;
 	long long int d;
@@ -16,8 +16,7 @@ int main(void)
 	printf("Size of a char: %d byte(s)\n", sizeof(a));
 	printf("Size of an int: %d byte(s)\n", sizeof(b));
 	printf("Size of a long int: %d byte(s)\n", sizeof(c));
-	printf("Size of a long long int: %d byte(s)\n", sizeof(d);
+	printf("Size of a long long int: %d byte(s)\n", sizeof(d));
 	printf("Size of a float: %d byte(s)\n", sizeof(e));
 	return (0);
-	
 }
