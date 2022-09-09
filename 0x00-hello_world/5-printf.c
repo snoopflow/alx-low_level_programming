abcf9221b54ed1974b03bdfadf972e4ac1acb369
snoopflow <mojolajesuolubode@gmail.com>
2022-09-08 20:37:28 -0700

    1-8

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Prints a text
+ *
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+        printf("\"with propper grammar, but the outcome is a peice of art,\n");
+        return (0);
+}
+
