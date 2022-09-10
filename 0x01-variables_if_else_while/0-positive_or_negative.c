42052ece762d880ccd7948c82e2ae4e6630822dd
snoopflow <mojolajesuolubode@gmail.com>
2022-09-09 21:19:45 -0700

    1-9

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,57 @@
 #include <stdlib.h>
+
 #include <time.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+#include <stdio.h>
+
+
+/**
+
+ * main - Prints if number is positive, zero or negative
+
+ *
+
+ * Return: Always (Success)
+
+ */
+
 int main(void)
-{
-	int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	return (0);
+{
+
+        int n;
+
+
+        srand(time(0));
+
+        n = rand() - RAND_MAX / 2;
+
+
+        if (n > 0)
+
+        {
+
+                printf("%d is positive\n", n);
+
+        }
+
+        else if (n == 0)
+
+        {
+
+                printf("%d is zero\n", n);
+
+        }
+
+        else
+
+        {
+
+                printf("%d is negative\n", n);
+
+        }
+
+
+        return (0);
+
 }
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,14 +1,59 @@
 #include <stdlib.h>
+
 #include <time.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+#include <stdio.h>
+
+/**
+
+ * main - Prints random number to variable
+
+ *
+
+ * Return: Always (Successful)
+
+ */
+
 int main(void)
-{
-	int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	return (0);
+{
+
+        int n;
+
+        char last[] = "Last digit of";
+
+
+        srand(time(0));
+
+        n = rand() - RAND_MAX / 2;
+
+        printf("%s %d is %d and is ", last, n, n % 10);
+
+        if (n % 10 > 5)
+
+        {
+
+                printf("greater than 5\n");
+
+        }
+
+        else if (n % 10 == 0)
+
+        {
+
+                printf("0\n");
+
+        }
+
+        else
+
+        {
+
+                printf("less than 6 and not 0\n");
+
+        }
+
+        return (0);
+
+
 }
